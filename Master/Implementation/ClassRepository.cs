d52b43abf00b2f4827778a3914e3079c0295d855
Ekerholten <marius@dogu.no>
2013-11-19 13:05:36 +0100

    Added classModel

---
+++ Master/Implementation/ClassRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Master.Models;
+
+namespace Master.Implementation
+{
+   public static class ClassRepository
+    {
+       private static readonly DomainDbContext _db = new DomainDbContext();
+       /*
+       public static List<ClassModel> GetAll()
+       {
+           return _db.ClassModels.ToList();
+       }
+
+       public static ClassModel GetByClassId(int id)
+       {
+           return _db.ClassModels.FirstOrDefault(x => x.Class == id);
+       }
+
+       public static ClassModel GetByDiagnose(string diagnose)
+       {
+           var intiFyed = Regex.Replace(diagnose, "[^0-9.]", "");
+           var diagnoseToInt = -9;
+           int.TryParse(intiFyed, out diagnoseToInt);
+           var result = _db.ClassModels.FirstOrDefault(x => x.ValueFrom <= diagnoseToInt && x.ValueTo >= diagnoseToInt);
+
+           return result;
+       }
+       */
+    }
+}
---
+++ Master/Models/ClassModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master.Models
+{
+   public class ClassModel
+    {
+       [Key]
+       public int Class { get; set; }
+
+       public int ValueFrom { get; set; }
+       public int ValueTo { get; set; }
+       public string Title { get; set; }
+       public string Description { get; set; }
+
+    }
+}
